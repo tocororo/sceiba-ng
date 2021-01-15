44b71f4bba453d88d748b92b4c26ee42480139e2
Rafael Martinez Estevez <rafael.martinez@upr.edu.cu>
2021-01-15 16:37:50 -0500

    environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,7 +2,7 @@ import { Environment } from 'toco-lib';
 
 class EnvironmentImpl implements Environment {
   production = false;
-  sceibaHost = 'https://sceiba.cu';
+  sceibaHost = 'https://sceiba.cu/';
   cuorHost = 'https://orgs.sceiba.cu/';
   sceibaApi = 'https://sceiba.cu/api/';
   cuorApi = 'https://orgs.sceiba.cu/api/';
