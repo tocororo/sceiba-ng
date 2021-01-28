0f11f4b13a7aced18ab10d45553a6018a4be73a1
Luis Enrique Cabezas Curbelo <lecc628@gmail.com>
2021-01-28 13:26:10 -0500

    Set comment for including the lazy loading.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
+
 import { HttpClient } from '@angular/common/http';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+
 import { MarkdownModule } from 'ngx-markdown';
+
 import { HomeComponent } from './home/home.component';
 import { RecordResolverService } from './record-resolver.service';
 import { RecordViewComponent } from './record-view/record-view.component';
@@ -10,74 +13,70 @@ import { StaticPagesComponent } from './static-pages/static-pages.component';
 import { UserProfileEditComponent } from './user/user-profile-edit/user-profile-edit.component';
 import { UserProfileComponent } from './user/user-profile/user-profile.component';
 
-
 const routes: Routes = [
-
-  {
-    path: '',
-    component: HomeComponent,
-  },
-  {
-		path:':uuid/view',
-		component: RecordViewComponent,
+	{
+		path: '',
+		component: HomeComponent,
+	},
+	{
+		path: ':uuid/view',
+		component: RecordViewComponent,  // TODO: poner esto en un módulo aparte y que use lazy loading (loadChildren). 
 		resolve: {
 			'record': RecordResolverService
 		}
 	},
-  {
+	{
 		path: 'search',
-		component: SearchComponent
-  },
-  {
-		path: 'search',
-		component: SearchComponent
-  },
-  {
-    path: 'profile',
-    children: [
-      {
-        path: '',
-        component: UserProfileComponent
-      },
-      {
-        path: 'edit',
-        component: UserProfileEditComponent
-      }
-    ],
-    canActivate: []
+		component: SearchComponent  // TODO: poner esto en un módulo aparte y que use lazy loading (loadChildren). 
 	},
-  {
-    path: 'faq',
-    component: StaticPagesComponent,
-    data: { src: 'assets/markdown/faq.md', title: 'FAQ' }
-  },
-  {
-    path: 'about',
-    component: StaticPagesComponent,
-    data: { src: 'assets/markdown/about.md', title: 'Sobre Nosotros' }
-  },
-  {
-    path: 'help',
-    component: StaticPagesComponent,
-    data: { src: 'assets/markdown/help.md', title: 'Ayuda' }
-  },
-  {
-    path: 'contact',
-    component: StaticPagesComponent,
-    data: { src: 'assets/markdown/contact.md', title: 'Contacto' }
-  },
-  {
-    path: 'policy',
-    component: StaticPagesComponent,
-    data: { src: 'assets/markdown/policy.md', title: 'Política de Privacidad' }
-  },
+	{
+		path: 'profile',  // TODO: poner esto en un módulo aparte y que use lazy loading (loadChildren). 
+		children: [
+			{
+				path: '',
+				component: UserProfileComponent
+			},
+			{
+				path: 'edit',
+				component: UserProfileEditComponent
+			}
+		],
+		canActivate: []
+	},
+
+	// TODO: poner los StaticPages en un módulo aparte y que use lazy loading (loadChildren). 
+	{
+		path: 'faq',
+		component: StaticPagesComponent,
+		data: { src: 'assets/markdown/faq.md', title: 'FAQ' }
+	},
+	{
+		path: 'about',
+		component: StaticPagesComponent,
+		data: { src: 'assets/markdown/about.md', title: 'Sobre Nosotros' }
+	},
+	{
+		path: 'help',
+		component: StaticPagesComponent,
+		data: { src: 'assets/markdown/help.md', title: 'Ayuda' }
+	},
+	{
+		path: 'contact',
+		component: StaticPagesComponent,
+		data: { src: 'assets/markdown/contact.md', title: 'Contacto' }
+	},
+	{
+		path: 'policy',
+		component: StaticPagesComponent,
+		data: { src: 'assets/markdown/policy.md', title: 'Política de Privacidad' }
+	},
 ];
 
 @NgModule({
-  imports: [
-    RouterModule.forRoot(routes),
-    MarkdownModule.forRoot({ loader: HttpClient })
-  ],
-  exports: [RouterModule, MarkdownModule]
+	imports: [
+		RouterModule.forRoot(routes),
+		MarkdownModule.forRoot({ loader: HttpClient })
+	],
+	exports: [RouterModule, MarkdownModule]
 })
 export class AppRoutingModule { }
